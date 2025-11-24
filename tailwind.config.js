a5f5aafdd222ca5dcc8bb3bece5fa1a3115d880a
Kate Yang <150059882+kateyang1998@users.noreply.github.com>
2025-11-24 13:27:53 -0500

    fix: customized responsive breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ export default {
   ],
   theme: {
     extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1100px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
   },
   plugins: [],
 };
